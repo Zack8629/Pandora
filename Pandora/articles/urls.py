d704faa350e703d15daba07d1d8476b55f44ea74
Dmitry R <dmr.main@outlook.com>
2022-02-15 19:43:41 +0300

    minor changes

--- Pandora/articles/urls.py
+++ Pandora/articles/urls.py
@@ -7,6 +7,5 @@ app_name = 'articles'
 urlpatterns = [
     path('', articles.articles_all, name='index_articles'),
     path('<slug:slug>/', articles.article_view, name='article_view'),
-    # path('hub_<int:pk>/', articles.category_view, name='category_view'),
     path('categories/<slug:slug>/', articles.CategoryDetail.as_view(), name='category_view'),
 ]
