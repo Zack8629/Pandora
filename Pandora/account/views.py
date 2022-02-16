d5b3f3961692ffeabc504dc661a2736f763f3dca
Dmitry R <dmr.main@outlook.com>
2022-02-16 21:37:06 +0300

    create article works correctly

--- Pandora/account/views.py
+++ Pandora/account/views.py
@@ -55,15 +55,15 @@ class AccountDetailView(ProperUserMixin, DetailView):
         return context
 
 
-class CreateCategoryView(ProperUserMixin, CreateView):
+class CreateCategoryView(CreateView):
     model = Category
     fields = ['title']
     template_name = 'account/category_create_form.html'
     success_url = reverse_lazy('account:create_category')
 
 
-class CreateArticlesView(ProperUserMixin, CreateView):
+class CreateArticlesView(CreateView):
     model = Articles
-    fields = ['title', 'content', 'category']
+    fields = ['title', 'content', 'category', 'author']
     template_name = 'account/articles_create_form.html'
     success_url = reverse_lazy('account:create_articles')
