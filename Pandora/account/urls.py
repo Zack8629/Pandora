b3929c58dee75c165b2ac9307ce72ea5d23fc00c
Dmitry R <dmr.main@outlook.com>
2022-02-23 19:43:12 +0300

    редактирование данных в личном кабинете

--- Pandora/account/urls.py
+++ Pandora/account/urls.py
@@ -7,9 +7,8 @@ app_name = 'account'
 
 urlpatterns = [
     path('login/', LoginView.as_view(next_page='/'), name='login'),
+    path('update/<int:pk>/', account.AccountUpdateView.as_view(), name='update'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('registration/', account.RegistrationView.as_view(), name='registration'),
     path('<int:pk>/', account.AccountDetailView.as_view(), name='account'),
-
-
 ]
