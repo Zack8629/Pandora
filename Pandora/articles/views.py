4641868c0aaa7273790033d2ba0ec34308ce7da8
Melyxi <58004428+Melyxi@users.noreply.github.com>
2022-03-18 12:57:52 +0300

    Update views.py

--- Pandora/articles/views.py
+++ Pandora/articles/views.py
@@ -49,7 +49,6 @@ class ArticlesListView(ContextDataMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = get_all_categories()
-        context['articles'] = Articles.objects.filter(published=True)
         if self.page_title is not None:
             context['page_title'] = self.page_title
         return context
